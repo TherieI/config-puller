c12b2f5e447407106eac6724815e58e8f79475c6
Gabriel Rosas <TheRealTheriel@gmail.com>
2022-06-03 16:15:59 -0800

    Added console log

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from tkinter import Tk, Frame, Label, Button, Entry, Scrollbar, Listbox
+from tkinter import Tk, Frame, Label, Button, Entry, Scrollbar, Listbox, Text
+from tkinter.scrolledtext import ScrolledText
 from tkinter import END, HORIZONTAL
 from tkinter.ttk import Progressbar, Combobox
 from tkinter.messagebox import showerror, showinfo
 from serial import Serial
 from serial.tools.list_ports import comports
 from threading import Thread
-from time import time
+from serial.serialutil import SerialException
 
 
 class App(Frame):
@@ -43,27 +44,27 @@ class App(Frame):
         # Entry to write a command to be executed
         add_dx = 300
         entry_cmd = Entry(self.master, text="red", fg="green")
-        entry_cmd.place(x=50+add_dx, y=70, width=200, height=25)
+        entry_cmd.place(x=50 + add_dx, y=70, width=200, height=25)
         self.widgets["entry_cmd"] = entry_cmd
 
         # Button to add a command to be executed
         btn_addcmd = Button(self.master, text="Add", command=self.add_cmd)
-        btn_addcmd.place(x=10+add_dx, y=70)
+        btn_addcmd.place(x=10 + add_dx, y=70)
         self.widgets["btn_addcmd"] = btn_addcmd
 
         scroll_dy = 100
         # List of commands to be executed
         scroll_cmdlist = Scrollbar(self.master)
-        scroll_cmdlist.place(x=300, y=10+scroll_dy, width=270, height=140)
+        scroll_cmdlist.place(x=300, y=10 + scroll_dy, width=270, height=140)
         listbox_cmdlist = Listbox(self.master, yscrollcommand=scroll_cmdlist.set)
-        listbox_cmdlist.place(x=300, y=30+scroll_dy, width=270, height=100)
+        listbox_cmdlist.place(x=300, y=30 + scroll_dy, width=270, height=100)
         scroll_cmdlist.config(command=listbox_cmdlist.yview)
         self.widgets["scroll_cmdlist"] = scroll_cmdlist
         self.widgets["lb_cmdlist"] = listbox_cmdlist
 
         # Button to clear listbox
         btn_clear = Button(self.master, text="Clear", command=self.clear_cmds)
-        btn_clear.place(x=300, y=160+scroll_dy, width=270, height=20)
+        btn_clear.place(x=300, y=160 + scroll_dy, width=270, height=20)
 
         # Combobox with label to select ports
         lb_ports = Label(self.master, text="Settings")
@@ -74,7 +75,7 @@ class App(Frame):
         lb_ports = Label(self.master, text="Select Port:")
         lb_ports.place(x=10, y=settings_dy)
         cb_ports = Combobox(self.master)
-        cb_ports.place(x=130, y=settings_dy-5, width=150, height=30)
+        cb_ports.place(x=130, y=settings_dy - 5, width=150, height=30)
         cb_ports["values"] = [port.name for port in comports()]
         cb_ports.current(0)
         self.widgets["cb_ports"] = cb_ports
@@ -83,15 +84,29 @@ class App(Frame):
         lb_hostname = Label(self.master, text="Hostname of Device: ")
         lb_hostname.place(x=10, y=40 + settings_dy)
         # Entry to input hostname of router
-        entry_rhostname = Entry(self.master, text="router", fg="green")
+        entry_rhostname = Entry(self.master, text="Router", fg="green")
         entry_rhostname.place(x=130, y=40 + settings_dy, width=150, height=25)
         self.widgets["entry_rhostname"] = entry_rhostname
 
         # Progressbar
         pb_progress = Progressbar(self.master, orient=HORIZONTAL, length=100, mode='determinate')
-        pb_progress.place(x=10, y=450, width=self.x-20, height=30)
+        pb_progress.place(x=10, y=450, width=self.x - 20, height=30)
         self.widgets["pb_progress"] = pb_progress
 
+        # Console Log
+        log_scroll_y = 140
+        log_scroll_x = 12
+        scroll_log = ScrolledText(self.master,
+                                  width=30,
+                                  height=8,
+                                  font=("Courier New", 8),
+                                  state="disabled"
+                                  )
+        scroll_log.place(x=log_scroll_x, y=10 + log_scroll_y, width=270, height=180)
+        self.widgets["scroll_log"] = scroll_log
+    # GUI ABOVE
+
+    # LOGIC BELOW
     def add_cmd(self):
         cmd = self.widgets["entry_cmd"].get()
         self.commands.append(cmd)
@@ -112,37 +127,44 @@ class App(Frame):
             extr_thread.start()
         else:
             showerror(title="Error", message="You are already in the process of extracting")
-# GUI ABOVE
 
-# LOGIC BELOW
     def _extract(self):
-        self.master.geometry(f"{self.x}x{self.y+100}")
+        self.master.geometry(f"{self.x}x{self.y + 100}")
         total = len(self.commands) + 1
         complete = 0
         result = ""
-        with Serial(self.get_port(), 9600, timeout=5) as ser:
-            print("Establishing a connection with the router")
+        try:
+            with Serial(self.get_port(), 9600, timeout=5) as ser:
+                self.log("Establishing a connection with the router...")
 
-            complete += 1
-            self.progress = complete / total * 100
-            self.widgets["pb_progress"]["value"] = int(self.progress)
-
-            self.write_cmd(ser, "enable")  # enter privilege exec mode
-            self.write_cmd(ser, "terminal length 0")  # commands will not pause
-            for cmd in self.commands:
-                print(f"starting {cmd}")
-                result += self.read_until_end(ser, cmd)
                 complete += 1
-                self.progress = complete/total * 100
+                self.progress = complete / total * 100
                 self.widgets["pb_progress"]["value"] = int(self.progress)
-                print(f"{self.progress}%")
 
+                self.log("Entering exec mode...")
+                self.write_cmd(ser, "enable")  # enter privilege exec mode
+                self.write_cmd(ser, "terminal length 0")  # commands will not pause
+                self.log("Entered exec mode")
+
+                for cmd in self.commands:
+                    self.log(f"Running command '{cmd}'")
+                    result += self.read_until_end(ser, cmd)
+                    complete += 1
+                    self.progress = complete / total * 100
+                    self.widgets["pb_progress"]["value"] = int(self.progress)
+
+        except SerialException as err:
+            self.log(f"Error: {err}\n*Cannot open multiple serial connections*")
+            self.extracting = False
+            return
         with open("config.txt", "w") as config:
             config.write(result)
         self.widgets["pb_progress"]["value"] = 0
         self.master.geometry(f"{self.x}x{self.y}")
         self.extracting = False
-        showinfo(title="Build complete", message="Commands extracted")
+        self.log("==================\n"
+                 "COMMANDS EXTRACTED\n"
+                 "==================")
 
     def get_port(self) -> str:
         port = self.widgets["cb_ports"].get()
@@ -161,10 +183,19 @@ class App(Frame):
         self.write_cmd(ser, cmd)
         output = ""
         line = ""
+        timeout = 3
         router_name = self.widgets["entry_rhostname"].get()
-        while line != f"{router_name}#":
-            # print(f"{line=}")
+        while line != f"{router_name}#" and timeout > 0:
             line = ser.readline().decode()
+            self.log(f"Reading line: {line[:-2]}")
             if "!" not in line and line[:-2] != "":
                 output += f"{line[:-2]}\n"  # get line without "\r\n"
+            elif line[:-2] == "":
+                timeout -= 1
         return output[:-6]  # clear "Route" bit at end
+
+    def log(self, txt: str):
+        print(txt)
+        self.widgets["scroll_log"].config(state="normal") # for readonly
+        self.widgets["scroll_log"].insert(END, txt + "\n")
+        self.widgets["scroll_log"].config(state="disabled")
